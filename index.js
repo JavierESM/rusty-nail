21f112731a13d15f9cd8c478d6edcdfbe4b245e4
hormaecheagustavo <donmariodistribuciones@gmail.com>
2020-10-13 21:29:45 -0300

    formulario de contacto carro de venta

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-const express = require ("express")
-const app = express()
-const path = require ("path")
-app.listen(3000, () => console.log("Server up"))
-app.get('/', (req, res) => {
-    let file = path.resolve('index.html')
-    res.sendFile(file)
-})
-app.get('/registro', (req, res) => {
-    let file = path.resolve('register.html')
-    res.sendFile(file)
-})
-app.get('/menu', (req, res) => {
-    let file = path.resolve('menu.html')
-    res.sendFile(file)
-})
-app.get('/bartender', (req, res) => {
-    let file = path.resolve('bartender.html')
-    res.sendFile(file)
-})
-app.get('/ejemplo', (req, res) => {
-    let file = path.resolve('ejemplo.html')
-    res.sendFile(file)
-})
+const express = require("express");
+const app = express();
+const path = require("path");
+app.listen(3000, () => console.log("Server up"));
+app.get("/", (req, res) => {
+  let file = path.resolve("index.html");
+  res.sendFile(file);
+});
+app.get("/registro", (req, res) => {
+  let file = path.resolve("register.html");
+  res.sendFile(file);
+});
+app.get("/menu", (req, res) => {
+  let file = path.resolve("menu.html");
+  res.sendFile(file);
+});
+app.get("/bartender", (req, res) => {
+  let file = path.resolve("bartender.html");
+  res.sendFile(file);
+});
+app.get("/ejemplo", (req, res) => {
+  let file = path.resolve("ejemplo.html");
+  res.sendFile(file);
+});
+app.get("/carrito-rn", (req, res) => {
+  let file = path.resolve("vistas/carrito-rn.html");
+  res.sendFile(file);
+});
+app.get("/contacto", (req, res) => {
+  let file = path.resolve("vistas/rn.html");
+  res.sendFile(file);
+});
+
 app.get(`*`, (req, res) => {
-    if (req.url.endsWith('.css')) {
-        let file = path.resolve('styles' + req.url)
-        return res.sendFile(file)
-    }
+  if (req.url.endsWith(".css")) {
+    let file = path.resolve("styles" + req.url);
+    return res.sendFile(file);
+  }
 
-let images = ['jpg', 'jpeg', 'gif', 'png', 'svg', 'gif']
-let ext = req.url.split('.')[1]
+  let images = ["jpg", "jpeg", "gif", "png", "svg", "gif"];
+  let ext = req.url.split(".")[1];
 
-if (images.includes(ext)) {
-    let file = path.resolve('public' + req.url)
-    return res.sendFile(file)
-}} 
-) 
+  if (images.includes(ext)) {
+    let file = path.resolve("public" + req.url);
+    return res.sendFile(file);
+  }
+});
