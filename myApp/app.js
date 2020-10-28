a27bbf5aa0bd1382a6cabb80f812d671a2a723a2
Sebastian Costa <sebacosta8822@gmail.com>
2020-10-28 09:31:14 -0300

    se agrega pagina de register y se organiza las rutas

--- myApp/app.js
+++ myApp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var productRouter = require('./routes/product');
+var registerRouter = require ('./routes/register');
 
 var app = express();
 
@@ -27,7 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/product', productRouter);
-
+app.use('/register',registerRouter);
 
 
 // catch 404 and forward to error handler
