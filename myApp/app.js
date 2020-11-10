985166b8c50814de64e479d3501d3b382f67c51e
Sebastian Costa <sebacosta8822@gmail.com>
2020-11-10 20:17:00 -0300

    put

--- myApp/app.js
+++ myApp/app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var methodOverride = require ("method-override");
 var usersRouter = require("./routes/users");
 var loginRouter = require("./routes/login");
 var productRouter = require("./routes/product");
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'))
 
 app.use(express.static(path.join(__dirname, "public")));
 
