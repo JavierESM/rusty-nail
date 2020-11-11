8eee74a5e77dc0adc3949e85cafb601a9b21c5d7
JavierESM <javierestebanmn@gmail.com>
2020-11-10 21:14:42 -0300

    algo

--- myApp/controllers/menuControllers.js
+++ myApp/controllers/menuControllers.js
@@ -3,16 +3,16 @@ const fs = require("fs")
 const productsFilePath = path.join(__dirname, '../data/products.json');
 let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+var toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 
 
 const menuControllers = {
   menu: function (req, res) {
-    res.render("menu");
+    res.render("menu", {products, toThousand});
   },
   menuNuestros: function (req, res) {
-
+  
     res.render("menu", {resultado})
   },
   menuClasicos: function (req, res) {
